5f90c7de8030896364091f5469d73255ce9464df
jorge-olvera <martynez.jorge456@gmail.com>
2021-04-09 01:13:50 -0500

    cambio de cache

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * cambios en tu sitio, para
  * depués actualizar este archivo.
  */
-const CACHE = "dmppwa-2.03";
+const CACHE = "jorgeolvera-2.03";
 
 /** Archivos requeridos para que
  * la aplicación funcione fuera de
